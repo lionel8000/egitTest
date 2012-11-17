eabde4b6dc672b75b4ddb20036d4b3d53e72619c
lionel <8836272@qq.com>
2012-11-17 19:28:56 +0800

    only the jdbc file

---
+++ ono/src/oo/OOOS.java
@@ -0,0 +1,29 @@
+package oo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class OOOS {
+
+	/**
+	 *
+	 * @param args
+	 * @author xjs
+	 * @date 2012-11-16 下午12:42:24
+	 */
+	public static void main(String[] args) throws Exception {
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection conn=DriverManager.getConnection("jdbc:oracle:thin:@127.0.0.1:1521:orcl", "book", "book");
+		Statement stm=conn.createStatement();
+//		String sql="delete from user where userId='"+1+"'";
+//		stm.executeUpdate(sql);
+		ResultSet rs = stm.executeQuery("select * from employees");
+		while(rs.next()){
+		System.out.println("ok ....");
+		}
+
+	}
+
+}
